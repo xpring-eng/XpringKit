cef8942aef360402bc71a9e55fe69420519dbde5
Keefer Taylor <keefer.t@gmail.com>
2019-08-19 17:38:27 -0400

    Wire up a network client on XpringClient

--- XpringKit/XpringClient.swift
+++ XpringKit/XpringClient.swift
@@ -1,5 +1,32 @@
+public protocol NetworkClient {
+    func getAccountInfo(_ request: AccountInfoRequest) throws -> AccountInfo
+}
+
+extension RippledServiceClient: NetworkClient {
+}
+
+public typealias Address = String
+
+/// An interface into the Xpring Platform.
 public class XpringClient {
-    public func test() {
-        print("Hello, XpringClient")
+    /// A network client that will make and receive requests.
+    private let networkClient: NetworkClient
+
+    public init(networkClient: NetworkClient) {
+        self.networkClient = networkClient
+    }
+
+    public func getBalance(for address: Address) throws -> XRPAmount {
+        let accountInfoRequest = AccountInfoRequest.with {
+            $0.address = address
+        }
+
+        let accountInfo = try networkClient.getAccountInfo(accountInfoRequest)
+        let accountData = accountInfo.accountData
+        let drops = accountData.balance
+
+        return XRPAmount.with {
+            $0.drops = drops
+        }
     }
 }
