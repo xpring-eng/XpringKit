a9666120d248cd0bce91045c7ae84d175c50fcb2
Keefer Taylor <keefer.t@gmail.com>
2019-08-20 16:59:53 -0400

    fix name in comments

--- XpringKit/NetworkClient.swift
+++ XpringKit/NetworkClient.swift
@@ -1,8 +1,8 @@
 /// A client which can make requests to the Xpring Network.
 public protocol NetworkClient {
-    /// Get an account info object.
+    /// Get an `AccountInfo` object.
     ///
-    /// - Parameter request: Request parameters for an account info.
+    /// - Parameter request: Request parameters for an `AccountInfo`.
     /// - Throws: An error if there was a problem communicating with the Xpring Network.
     /// - Returns: An AccountInfo object that corresponds to the request.
     func getAccountInfo(_ request: AccountInfoRequest) throws -> AccountInfo
