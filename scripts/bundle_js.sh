d7277b8cfad573efdb453110aeb22f4b532ac3a3
Keefer Taylor <keefer.t@gmail.com>
2019-09-09 16:22:50 -0700

    Script to bundle js

---
+++ scripts/bundle_js.sh
@@ -0,0 +1,18 @@
+WORKING_DIR=$(pwd)
+
+echo "Bundling JS"
+cd terram
+
+echo ">> Installing Node Dependencies"
+npm i
+echo ">> Done Installing Node Dependencies"
+
+echo ">> Running Webpack."
+npm run webpack
+echo ">> Done Running Webpack"
+
+echo "Done Bundling JS"
+
+cd $WORKING_DIR
+echo "Copying Artifacts"
+cp terram/dist/bundled.js ./XpringKit/Resources/
