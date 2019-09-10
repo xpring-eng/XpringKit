7b436536138a1e84fc8568ef6193a278c2c656fe
Keefer Taylor <keefer.t@gmail.com>
2019-09-09 17:58:17 -0700

    Passing isValidAddress tests

--- XpringKit/Resources/primitives.js
+++ XpringKit/Resources/primitives.js
@@ -1,3 +1,4 @@
 function xpring() {
     return "xpring"
 }
+jav
--- XpringKit/Terram.swift
+++ XpringKit/Terram.swift
@@ -2,32 +2,52 @@ import Foundation
 import JavaScriptCore
 
 public class Terram {
+    /// The javascript context.
     private let context: JSContext
 
+    /// Terram objects
+    private let isValidAddressFunction: JSValue
+
     public init?() {
-        guard let context = JSContext() else {
-            return nil
-        }
-
         let bundle = Bundle(for: type(of: self))
-        guard let fileURL = bundle.url(forResource: "primitives", withExtension: "js") else {
-            return nil
-        }
 
-        guard let javascript = try? String(contentsOf: fileURL) else {
+        guard
+            let context = JSContext(),
+            let fileURL = bundle.url(forResource: "bundled", withExtension: "js"),
+            let javascript = try? String(contentsOf: fileURL)
+        else {
             return nil
         }
 
         context.evaluateScript(javascript)
 
-        self.context = context
-    }
-
-    public func xpring() -> String? {
-        guard let xpringFn = context.objectForKeyedSubscript("xpring") else {
+        guard
+            let entrypoint = context.objectForKeyedSubscript("EntryPoint"),
+            let `default` = entrypoint.objectForKeyedSubscript("default"),
+            let utils = `default`.objectForKeyedSubscript("Utils"),
+            let isValidAddressFunction = utils.objectForKeyedSubscript("isValidAddress"),
+            !isValidAddressFunction.isUndefined
+        else {
             return nil
         }
 
-        return xpringFn.call(withArguments: nil)?.toString()
+        self.context = context
+
+        self.isValidAddressFunction = isValidAddressFunction
     }
+
+    public func isValid(address: String) -> Bool {
+        let result = isValidAddressFunction.call(withArguments: [ address ])!
+        return result.toBool()
+    }
+
+//    public func xpring() -> String? {
+//            let
+//            let result = xpringFn.call(withArguments: nil)
+//        else {
+//            return nil
+//        }
+//
+//        return result.toString()
+//    }
 }
