7ab9246b1b565ddae88bb707b4f2c7a88970513a
Keefer Taylor <keefer.t@gmail.com>
2019-10-21 10:30:46 -0500

    Re-bundle JS and add tests for X-Addresses

--- Tests/UtilsTest.swift
+++ Tests/UtilsTest.swift
@@ -2,17 +2,25 @@ import XCTest
 import XpringKit
 
 class UtilsTest: XCTestCase {
-	func testIsValidAddressValidAddress() {
+	func testIsValidAddressValidClassicAddress() {
 		XCTAssertTrue(Utils.isValid(address: "rU6K7V3Po4snVhBBaU29sesqs2qTQJWDw1"))
 	}
+
+    func testIsValidAddressValidXAddress() {
+        XCTAssertTrue(Utils.isValid(address: "XVLhHMPHU98es4dbozjVtdWzVrDjtV18pX8yuPT7y4xaEHi"))
+    }
 
 	func testIsValidAddressInvalidAlphabet() {
 		XCTAssertFalse(Utils.isValid(address: "1EAG1MwmzkG6gRZcYqcRMfC17eMt8TDTit"))
 	}
 
-	func testIsvValidAddressInvlalidChecksum() {
+	func testIsvValidAddressInvalidChecksumClassicAddress() {
 		XCTAssertFalse(Utils.isValid(address: "rU6K7V3Po4sBBBBBaU29sesqs2qTQJWDw1"))
 	}
+
+    func testIsvValidAddressInvalidChecksumXAddress() {
+        XCTAssertFalse(Utils.isValid(address: "XVLhHMPHU98es4dbozjVtdWzVrDjtV18pX8yuPT7y4xaEHI"))
+    }
 
 	func testIsValidAddressInvalidCharacters() {
 		XCTAssertFalse(Utils.isValid(address: "rU6K7V3Po4sBBBBBaU@#$%qs2qTQJWDw1"))
