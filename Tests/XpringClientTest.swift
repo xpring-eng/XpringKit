9f9a08a361efe34657d9bbfc0b2156fcd73bde2f
Keefer Taylor <keefer.t@gmail.com>
2019-11-19 21:36:59 -0500

    remove other big int

--- Tests/XpringClientTest.swift
+++ Tests/XpringClientTest.swift
@@ -6,7 +6,6 @@ import XCTest
 final class XpringClientTest: XCTestCase {
 	static let wallet = Wallet(seed: "snYP7oArxKepd3GPDcrjMsJYiJeJB")!
 	static let destinationAddress = "XVfC9CTCJh6GN2x8bnrw3LtdbqiVCUFyQVMzRrMGUZpokKH"
-	static let sendAmount = BigUInt(stringLiteral: "20")
 
 	// MARK: - Balance
 	func testGetBalanceWithSuccess() {
