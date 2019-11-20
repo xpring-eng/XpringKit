622fc03fe6ea15943f93f92b986157153fea1951
Keefer Taylor <keefer.t@gmail.com>
2019-11-19 21:14:26 -0500

    remove bigint

--- Tests/XpringClientTest.swift
+++ Tests/XpringClientTest.swift
@@ -8,8 +8,6 @@ final class XpringClientTest: XCTestCase {
 	static let destinationAddress = "XVfC9CTCJh6GN2x8bnrw3LtdbqiVCUFyQVMzRrMGUZpokKH"
 	static let sendAmount = BigUInt(stringLiteral: "20")
 
-	static let balance = BigUInt(stringLiteral: "1000")
-
 	// MARK: - Balance
 	func testGetBalanceWithSuccess() {
 		print("hello world")
