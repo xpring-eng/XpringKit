6a579d3d22e1b40eb43f7a7191e9c1841899fb15
Keefer Taylor <keefer.t@gmail.com>
2019-11-19 21:00:49 -0500

    re-add

--- Tests/XpringClientTest.swift
+++ Tests/XpringClientTest.swift
@@ -4,6 +4,8 @@ import XCTest
 @testable import XpringKit
 
 final class XpringClientTest: XCTestCase {
+	static let wallet = Wallet(seed: "snYP7oArxKepd3GPDcrjMsJYiJeJB")!
+	static let destinationAddress = "XVfC9CTCJh6GN2x8bnrw3LtdbqiVCUFyQVMzRrMGUZpokKH"
 	static let sendAmount = BigUInt(stringLiteral: "20")
 
 	static let balance = BigUInt(stringLiteral: "1000")
