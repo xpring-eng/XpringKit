044f6fdfcd7bd3b8cd9f51321b90d4b0ed736249
Denis Angell <denisangell@harpangell.com>
2020-01-22 09:21:54 -0800

    Add Transaction Status as String init

--- XpringKit/TransactionStatus.swift
+++ XpringKit/TransactionStatus.swift
@@ -1,14 +1,14 @@
 /// Represents statuses of transactions.
-public enum TransactionStatus {
+public enum TransactionStatus: String {
     // The transaction was included in a finalized ledger and failed.
     case failed
-    
+
     // The transaction is not included in a finalized ledger.
     case pending
-    
+
     // The transaction was included in a finalized ledger and succeeded.
     case succeeded
-    
+
     // The transaction status is unknown.
     case unknown
 }
