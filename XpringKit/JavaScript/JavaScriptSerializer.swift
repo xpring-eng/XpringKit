03a7b3d85024981c6356ffd638548c6b21d0f81d
Denis Angell <denisangell@harpangell.com>
2020-01-22 09:24:05 -0800

    Add New Buffer Serializer

--- XpringKit/JavaScript/JavaScriptSerializer.swift
+++ XpringKit/JavaScript/JavaScriptSerializer.swift
@@ -7,22 +7,22 @@ internal class JavaScriptSerializer {
         public static let transaction = "Transaction"
         public static let wallet = "Wallet"
     }
-    
+
     /// References to JavaScript functions.
     private let deserializeTransactionFunction: JSValue
-    
+
     /// References to JavaScript classes.
     private let walletClass: JSValue
-    
+
     /// Initialize a new JavaScriptSerializer.
     // TODO(keefertaylor): Context needs to get injected so that objects don't move between contexts. Remove this injection when Context exists as a shared Singleton.
     public init(context: JSContext) {
         walletClass = XRPJavaScriptLoader.load(ResourceNames.wallet, from: context)
-        
+
         let transactionClass = XRPJavaScriptLoader.load(ResourceNames.transaction, from: context)
         deserializeTransactionFunction = XRPJavaScriptLoader.load(ResourceNames.deserializeBinary, from: transactionClass)
     }
-    
+
     /// Serialize a `Wallet` to a JavaScript object.
     ///
     /// - Parameter wallet: The `Wallet` to serialize.
@@ -30,12 +30,26 @@ internal class JavaScriptSerializer {
     public func serialize(wallet: Wallet) -> JSValue {
         return walletClass.construct(withArguments: [ wallet.publicKey, wallet.privateKey])
     }
-    
+
     /// Serialize a transaction to a JavaScript object.
     ///
     /// - Parameter transaction: The transaction to serialize.
     /// - Returns: A JSValue representing the transaction.
     public func serialize(transaction: Io_Xpring_Transaction) -> JSValue? {
+        guard
+            let transactionData = try? transaction.serializedData()
+            else {
+                return nil
+        }
+        let transactionBytes = [UInt8](transactionData)
+        return deserializeTransactionFunction.call(withArguments: [transactionBytes])!
+    }
+
+    /// Serialize a transaction to a JavaScript object.
+    ///
+    /// - Parameter transaction: The transaction to serialize.
+    /// - Returns: A JSValue representing the transaction.
+    public func serialize(transaction: Rpc_V1_Transaction) -> JSValue? {
         guard
             let transactionData = try? transaction.serializedData()
             else {
