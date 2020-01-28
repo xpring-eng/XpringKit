60c2ab3c51ab6ca71ac594ab9c69793b526ba7de
Keefer Taylor <keefer.t@gmail.com>
2020-01-27 20:35:17 -0500

    build out balance

--- Tests/DefaultXpringClientTest.swift
+++ Tests/DefaultXpringClientTest.swift
@@ -6,11 +6,43 @@ final class DefaultXpringClientTest: XCTestCase {
 
   // MARK: - Balance
 
-  func testGetBalance() {
-    // GIVEN a XpringClient.
-    let xpringClient = DefaultXpringClient()
+  func testGetBalanceWithSuccess() {
+    // GIVEN a Xpring client which will successfully return a balance from a mocked network call.
+    let xpringClient = DefaultXpringClient(networkClient: FakeNetworkClient.successfulFakeNetworkClient)
 
-    // WHEN the balance is retrieved THEN an error is thrown.
+    // WHEN the balance is requested.
+    guard let balance = try? xpringClient.getBalance(for: .destinationAddress) else {
+      XCTFail("Exception should not be thrown when trying to get a balance")
+      return
+    }
+
+    // THEN the balance is correct.
+    XCTAssertEqual(balance, .balance)
+  }
+
+  func testGetBalanceWithClassicAddress() {
+    // GIVEN a classic address.
+    guard let classicAddressComponents = Utils.decode(xAddress: .destinationAddress) else {
+      XCTFail("Failed to decode X-Address.")
+      return
+    }
+    let xpringClient = DefaultXpringClient(networkClient: FakeNetworkClient.successfulFakeNetworkClient)
+
+    // WHEN the balance is requested THEN an error is thrown.
+    XCTAssertThrowsError(try xpringClient.getBalance(for: classicAddressComponents.classicAddress))
+  }
+
+  func testGetBalanceWithFailure() {
+    // GIVEN a Xpring client which will throw an error when a balance is requested.
+    let networkClient = FakeNetworkClient(
+      accountInfoResult: .failure(XpringKitTestError.mockFailure),
+      feeResult: .success(.testGetFeeResponse),
+      submitTransactionResult: .success(.testSubmitTransactionResponse),
+      transactionStatusResult: .success(.testGetTxResponse)
+    )
+    let xpringClient = DefaultXpringClient(networkClient: networkClient)
+
+    // WHEN the balance is requested THEN the error is thrown.
     XCTAssertThrowsError(try xpringClient.getBalance(for: .testAddress))
   }
 }
---
+++ Tests/Helpers/Rpc_V1_GetTxResponse+Test.swift
@@ -0,0 +1,5 @@
+import XpringKit
+
+extension Rpc_V1_GetTxResponse {
+  static let testGetTxResponse = Rpc_V1_GetTxResponse()
+}
