99123c29b58095ecfd7f8071ad86caa5da6cc222
Keefer Taylor <keefer.t@gmail.com>
2020-01-27 22:26:32 -0500

    integration tests for read

--- Tests/IntegrationTests.swift
+++ Tests/IntegrationTests.swift
@@ -8,8 +8,11 @@ extension Wallet {
 }
 
 extension String {
-  /// The URL of the remote gRPC service.
-  public static let remoteURL = "grpc.xpring.tech:80"
+  /// The URL of the remote legacy gRPC service.
+  public static let legacyRemoteURL = "grpc.xpring.tech:80"
+
+  /// The URL of a remote rippled node with gRPC enabled.
+  public static let remoteURL = "3.14.64.116:50051"
 
   /// An address on the chain to receive funds.
   public static let recipientAddress = "X7cBcY4bdTTzk3LHmrKAK6GyrirkXfLHGFxzke5zTmYMfw4"
@@ -21,12 +24,15 @@ extension BigUInt {
 }
 
 extension TransactionHash {
-  public static let successfulTransactionHash = "9A88C8548E03958FD97AF44AE5A8668896D195A70CF3FF3CB8E57096AA717135"
+  public static let successfulTransactionHash = "DFDC135AE3C8F68A1DC3959CBCFCABEA3EB91BDF7F8D22468A4F4FE9350FE8DA"
 }
 
 /// Integration tests run against a live remote client.
 final class IntegrationTests: XCTestCase {
-  private let client = XpringClient(grpcURL: .remoteURL)
+  private let legacyClient = XpringClient(grpcURL: .legacyRemoteURL, useNewProtocolBuffers: false)
+  private let client = XpringClient(grpcURL: .remoteURL, useNewProtocolBuffers: true)
+
+  // MARK: - rippled Protocol Buffers
 
   func testGetBalance() {
     do {
@@ -36,17 +42,36 @@ final class IntegrationTests: XCTestCase {
     }
   }
 
-  func testSendXRP() {
+  func testGetTransactionStatus() {
     do {
-      _ = try client.send(.drops, to: .recipientAddress, from: .testWallet)
+      let transactionStatus = try client.getTransactionStatus(for: .successfulTransactionHash)
+      XCTAssertEqual(transactionStatus, .succeeded)
+    } catch {
+      XCTFail("Failed retrieving transaction hash with error: \(error)")
+    }
+  }
+
+  // MARK: - Legacy Protocol Buffers
+
+  func testGetBalance_legacy() {
+    do {
+      _ = try legacyClient.getBalance(for: Wallet.testWallet.address)
+    } catch {
+      XCTFail("Failed retrieving balance with error: \(error)")
+    }
+  }
+
+  func testSendXRP_legacy() {
+    do {
+      _ = try legacyClient.send(.drops, to: .recipientAddress, from: .testWallet)
     } catch {
       XCTFail("Failed sending XRP with error: \(error)")
     }
   }
 
-  func testGetTransactionStatus() {
+  func testGetTransactionStatus_legacy() {
     do {
-      let transactionStatus = try client.getTransactionStatus(for: .successfulTransactionHash)
+      let transactionStatus = try legacyClient.getTransactionStatus(for: .successfulTransactionHash)
       XCTAssertEqual(transactionStatus, .succeeded)
     } catch {
       XCTFail("Failed retrieving transaction hash with error: \(error)")
