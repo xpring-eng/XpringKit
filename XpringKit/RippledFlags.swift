49308ba70f03d53b132b9b9904d4a9298a1396fe
Keefer Taylor <keefer.t@gmail.com>
2020-02-19 13:44:03 -0500

    fix casing

--- XpringKit/RippledFlags.swift
+++ XpringKit/RippledFlags.swift
@@ -3,7 +3,7 @@
 /// These are only flags which are utilized in Xpring SDK. For a complete list of flags, see:
 /// https://xrpl.org/transaction-common-fields.html#flags-field.
 public struct RippledFlags: OptionSet {
-  public let rawValue: Uint32
+  public let rawValue: UInt32
 
   /// Flag values.
   public static let tfPartialPayment = RippledFlags(rawValue: 131_072)
