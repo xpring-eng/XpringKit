9f36b325dd387c81498fc10e65826e14ff96e5ce
Keefer Taylor <keefer.t@gmail.com>
2020-02-19 11:07:53 -0500

    add flags

---
+++ XpringKit/RippledFlags.swift
@@ -0,0 +1,15 @@
+/// Flags used in ripppled transactions.
+///
+/// - These are only flags which are utilized in Xpring SDK. For a complete list of flags, see:
+/// https://xrpl.org/transaction-common-fields.html#flags-field.
+public struct RippledFlags: OptionSet {
+  public let rawValue: Int
+
+  /// Flag values.
+  public static let tfPartialPayment = 131072
+
+  /// Initialize a new RippledFlag
+  public init(rawValue: Int) {
+    self.rawValue = rawValue
+  }
+}
