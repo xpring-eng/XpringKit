5ca38d9d9b2330d3a521498a2044e90d7697ef5f
Keefer Taylor <keefer.t@gmail.com>
2020-03-03 20:30:18 +0000

    pruning

--- XpringKit/XRPTransactionType.swift
+++
@@ -1,8 +0,0 @@
-/// Types of transactions on the XRP Ledger.
-///
-/// This is a partial list. Please file an issue if you have a use case that requires additional types.
-///
-/// - SeeAlso: https://xrpl.org/transaction-formats.html
-public enum XRPTransactionType {
-  case payment
-}
