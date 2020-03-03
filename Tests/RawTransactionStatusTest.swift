3a802631c1007893eb29e3e0c0d9cbed3c120f89
Keefer Taylor <keefer.t@gmail.com>
2020-03-03 15:04:31 +0000

    fix tests

--- Tests/RawTransactionStatusTest.swift
+++ Tests/RawTransactionStatusTest.swift
@@ -19,46 +19,48 @@ class RawTransactionStatusTest: XCTestCase {
   // MARK: - rippled Protocol Buffers
 
   func testNonPaymentIsFullPayment() {
-    // GIVEN a getTxResponse which is not a payment.
-    let getTxResponse = Rpc_V1_GetTxResponse.with {
-      $0.transaction = Rpc_V1_Transaction.with {
+    // GIVEN a getTransactionResponse which is not a payment.
+    let getTxResponse = Org_Xrpl_Rpc_V1_GetTransactionResponse.with {
+      $0.transaction = Org_Xrpl_Rpc_V1_Transaction.with {
         $0.transactionData = .none
       }
     }
 
     // WHEN the response is wrapped into a RawTransactionStatus object.
-    let rawTransactionStatus = RawTransactionStatus(getTxResponse: getTxResponse)
+    let rawTransactionStatus = RawTransactionStatus(getTransactionResponse: getTxResponse)
 
     // THEN the raw transaction status reports it is not a full payment.
     XCTAssertFalse(rawTransactionStatus.isFullPayment)
   }
 
   func testPartialPaymentIsFullPayment() {
-    // GIVEN a getTxResponse which is a payment with the partial payment flags set.
-    let getTxResponse = Rpc_V1_GetTxResponse.with {
-      $0.transaction = Rpc_V1_Transaction.with {
-        $0.payment = Rpc_V1_Payment()
-        $0.flags = RippledFlags.tfPartialPayment.rawValue
+    // GIVEN a getTransactionResponse which is a payment with the partial payment flags set.
+    let getTxResponse = Org_Xrpl_Rpc_V1_GetTransactionResponse.with {
+      $0.transaction = Org_Xrpl_Rpc_V1_Transaction.with {
+        $0.payment = Org_Xrpl_Rpc_V1_Payment()
+        $0.flags = Org_Xrpl_Rpc_V1_Flags.with {
+          $0.value = RippledFlags.tfPartialPayment.rawValue
+        }
       }
     }
 
     // WHEN the response is wrapped into a RawTransactionStatus object.
-    let rawTransactionStatus = RawTransactionStatus(getTxResponse: getTxResponse)
+    let rawTransactionStatus = RawTransactionStatus(getTransactionResponse: getTxResponse)
 
     // THEN the raw transaction status reports it is not a full payment.
     XCTAssertFalse(rawTransactionStatus.isFullPayment)
   }
 
   func testPaymentIsFullPayment() {
-    // GIVEN a getTxResponse which is a payment.
-    let getTxResponse = Rpc_V1_GetTxResponse.with {
-      $0.transaction = Rpc_V1_Transaction.with {
-        $0.payment = Rpc_V1_Payment()
+    // GIVEN a getTransactionResponse which is a payment.
+    let getTxResponse = Org_Xrpl_Rpc_V1_GetTransactionResponse.with {
+      $0.transaction = Org_Xrpl_Rpc_V1_Transaction.with {
+        $0.payment = Org_Xrpl_Rpc_V1_Payment()
       }
     }
 
     // WHEN the response is wrapped into a RawTransactionStatus object.
-    let rawTransactionStatus = RawTransactionStatus(getTxResponse: getTxResponse)
+    let rawTransactionStatus = RawTransactionStatus(getTransactionResponse: getTxResponse)
 
     // THEN the raw transaction status reports it is a full payment.
     XCTAssertTrue(rawTransactionStatus.isFullPayment)
