50ac4339be800bcefb0cc36f868fac88b27e33bf
Keefer Taylor <keefer.t@gmail.com>
2020-03-03 20:53:40 +0000

    updates

--- Tests/ProtocolBufferConversionTests.swift
+++ Tests/ProtocolBufferConversionTests.swift
@@ -9,6 +9,18 @@ extension Org_Xrpl_Rpc_V1_Currency {
   }
 }
 
+extension Org_Xrpl_Rpc_V1_Payment.PathElement {
+  static let testPathElement = Org_Xrpl_Rpc_V1_Payment.PathElement.with {
+    $0.account = Org_Xrpl_Rpc_V1_AccountAddress.with {
+      $0.address = "r123"
+    }
+    $0.currency = .testCurrency
+    $0.issuer = Org_Xrpl_Rpc_V1_AccountAddress.with {
+      $0.address = "r456"
+    }
+  }
+}
+
 /// Tests conversion of protocol buffer to native Swift structs.
 final class ProtocolBufferConversionTests: XCTestCase {
 
@@ -30,15 +42,7 @@ final class ProtocolBufferConversionTests: XCTestCase {
 
   func testConvertPathElementAllFieldsSet() {
     // GIVEN a PathElement protocol buffer with all fields set.
-    let pathElementProto = Org_Xrpl_Rpc_V1_Payment.PathElement.with {
-      $0.account = Org_Xrpl_Rpc_V1_AccountAddress.with {
-        $0.address = "r123"
-      }
-      $0.currency = .testCurrency
-      $0.issuer = Org_Xrpl_Rpc_V1_AccountAddress.with {
-        $0.address = "r456"
-      }
-    }
+    let pathElementProto = Org_Xrpl_Rpc_V1_Payment.PathElement.testPathElement
 
     // WHEN the protocol buffer is converted to a native Swift type.
     let pathElement = XRPPathElement(pathElement: pathElementProto)
@@ -60,5 +64,44 @@ final class ProtocolBufferConversionTests: XCTestCase {
     XCTAssertNil(pathElement.account)
     XCTAssertNil(pathElement.currency)
     XCTAssertNil(pathElement.issuer)
+  }
+
+  // MARK: - Org_Xrpl_Rpc_V1_Transaction.Path
+
+  func testConvertPathsWithNoPaths() {
+    // GIVEN a set of paths with zero paths.
+    let pathProto = Org_Xrpl_Rpc_V1_Payment.Path()
+
+    // WHEN the protocol buffer is converted to a native Swift type.
+    let path = XRPPath(path: pathProto)
+
+    // THEN there are zero paths in the output.
+    XCTAssertEqual(path.pathElements.count, 0)
+  }
+
+  func testConvertPathsWithOnePath() {
+    // GIVEN a set of paths with one path.
+    let pathProto = Org_Xrpl_Rpc_V1_Payment.Path.with {
+      $0.elements = [ .testPathElement ]
+    }
+
+    // WHEN the protocol buffer is converted to a native Swift type.
+    let path = XRPPath(path: pathProto)
+
+    // THEN there is one path in the output.
+    XCTAssertEqual(path.pathElements.count, 1)
+  }
+
+  func testConvertPathsWithManyPaths() {
+    // GIVEN a set of paths with one path.
+    let pathProto = Org_Xrpl_Rpc_V1_Payment.Path.with {
+      $0.elements = [ .testPathElement, .testPathElement, .testPathElement ]
+    }
+
+    // WHEN the protocol buffer is converted to a native Swift type.
+    let path = XRPPath(path: pathProto)
+
+    // THEN there are multiple paths in the output.
+    XCTAssertEqual(path.pathElements.count, 3)
   }
 }
