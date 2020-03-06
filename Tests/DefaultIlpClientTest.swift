4db3b1bf090b43b17e786a381537ca8d40860784
nkramer44 <noah.ph.kramer@gmail.com>
2020-03-06 10:37:49 -0500

    add test stubs

---
+++ Tests/DefaultIlpClientTest.swift
@@ -0,0 +1,24 @@
+import SwiftGRPC
+import XCTest
+@testable import XpringKit
+
+final class DefaultIlpClientTest: XCTestCase {
+
+    // MARK: - Balance
+    func testGetBalanceWithSuccess() {
+        // GIVEN an IlpClient which will successfully return a balance from a mocked network call.
+        
+        // WHEN the balance is requested
+        
+        // THEN the balance is correct
+    }
+
+    // MARK: - Payment
+    func testSendPaymentWithSuccess() {
+        // GIVEN an IlpClient which will successfully return a payment response from a mocked network call.
+        
+        // WHEN a payment is sent
+        
+        // THEN the payment response is correct
+    }
+}
