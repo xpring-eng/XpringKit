0555b519a1737ee79870d6cd34fff8aa7f6aa62a
Amie Corso <acorso@ripple.com>
2020-03-18 13:58:38 -0700

    update documentation in DefaultXRPClient

--- XpringKit/DefaultXRPClient.swift
+++ XpringKit/DefaultXRPClient.swift
@@ -228,7 +228,7 @@ extension DefaultXRPClient: XRPClientDecorator {
   /// - Note: This method only works for payment type transactions, see: https://xrpl.org/payment.html
   /// - Note: This method only returns the history that is contained on the remote node, which may not contain a full history of the network.
   ///
-  /// - Parameter address: The address to check the existence of.
+  /// - Parameter address: The address (account) for which to retrieve the payment history.
   /// - Throws: An error if there was a problem communicating with the XRP Ledger.
   /// - Returns: An array of payments associated with the account.
   public func paymentHistory(for address: Address) throws -> [XRPTransaction] {
