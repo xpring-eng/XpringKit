d2c397b5ff3604b3b1d2b959c98009d8c99a74cc
Keefer Taylor <keefer.t@gmail.com>
2020-03-31 16:43:54 +0900

    remove old protocol buffer initializer

--- XpringKit/RawTransactionStatus.swift
+++ XpringKit/RawTransactionStatus.swift
@@ -13,14 +13,6 @@ public struct RawTransactionStatus {
   /// Whether Xpring SDK can bucket this transaction status into a TransactionStatus enum.
   public let isFullPayment: Bool
 
-  /// Initialize a new `RawTransactionStatus` from an `Io_Xpring_TransactionStatus`.
-  public init(transactionStatus: Io_Xpring_TransactionStatus) {
-    self.validated = transactionStatus.validated
-    self.lastLedgerSequence = transactionStatus.lastLedgerSequence
-    self.transactionStatusCode = transactionStatus.transactionStatusCode
-    self.isFullPayment = true
-  }
-
   /// Initialize a new `RawTransactionStatus` from an `Org_Xrpl_Rpc_V1_GetTransactionResponse`.
   public init(getTransactionResponse: Org_Xrpl_Rpc_V1_GetTransactionResponse) {
     self.validated = getTransactionResponse.validated
