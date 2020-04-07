e6d93cf5e7290a281109d74a8c57def066532632
Keefer Taylor <keefer.t@gmail.com>
2020-04-08 01:45:53 +0900

    fix indentation

--- XpringKit/ILP/IlpError.swift
+++ XpringKit/ILP/IlpError.swift
@@ -2,46 +2,46 @@ import SwiftGRPC
 
 /// Errors that may occur when interacting with Xpring's ILP infrastructure
 public enum IlpError: Error {
-    /// The accountID that was provided does not exist.
-    case accountNotFound
+  /// The accountID that was provided does not exist.
+  case accountNotFound
 
-    /// Something went wrong on the ILP network
-    case internalError
+  /// Something went wrong on the ILP network
+  case internalError
 
-    /// The access token has an invalid format, likely starts with "Bearer ".
-    case invalidAccessToken
+  /// The access token has an invalid format, likely starts with "Bearer ".
+  case invalidAccessToken
 
-    /// The request had an invalid argument
-    case invalidArgument
+  /// The request had an invalid argument
+  case invalidArgument
 
-    /// Authentication failed on the requested connector.
-    case unauthenticated
+  /// Authentication failed on the requested connector.
+  case unauthenticated
 
-    /// The requested functionality is not yet implemented.
-    case unimplemented
+  /// The requested functionality is not yet implemented.
+  case unimplemented
 
-    /// An unknown error occurred
-    case unknown
+  /// An unknown error occurred
+  case unknown
 
-    /// Translate an Error thrown from an Ilp network client to an IlpError.
-    ///
-    /// gRPC services return an error with a status code,
-    /// so we need to map gRPC error status to native IlpErrors.
-    ///
-    /// - Parameters:
-    ///   - callResult: The CallResult of an RPCError returned by a network call.
-    public static func from(_ callResult: CallResult) -> IlpError {
-        switch callResult.statusCode {
-        case .notFound:
-            return IlpError.accountNotFound
-        case .unauthenticated:
-            return IlpError.unauthenticated
-        case .invalidArgument:
-            return IlpError.invalidArgument
-        case .internalError:
-            return IlpError.internalError
-        default:
-            return IlpError.unknown
-        }
+  /// Translate an Error thrown from an Ilp network client to an IlpError.
+  ///
+  /// gRPC services return an error with a status code,
+  /// so we need to map gRPC error status to native IlpErrors.
+  ///
+  /// - Parameters:
+  ///   - callResult: The CallResult of an RPCError returned by a network call.
+  public static func from(_ callResult: CallResult) -> IlpError {
+    switch callResult.statusCode {
+    case .notFound:
+      return IlpError.accountNotFound
+    case .unauthenticated:
+      return IlpError.unauthenticated
+    case .invalidArgument:
+      return IlpError.invalidArgument
+    case .internalError:
+      return IlpError.internalError
+    default:
+      return IlpError.unknown
     }
+  }
 }
