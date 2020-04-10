3cf84c34695f432b819c186c0d87e088f5c85e95
Keefer Taylor <keefer.t@gmail.com>
2020-04-10 21:25:28 +0900

    use fake results

--- XpringKit/XRPLedgerError.swift
+++ XpringKit/XRPLedgerError.swift
@@ -3,7 +3,7 @@ public enum XRPLedgerError: Error {
   /// A problem occurred while performing the cryptographic signing process.
   case signingError
 
-  /// An invalid address was given. The invalid addresss is provided as an associated value.
+  /// An invalid address was given. The invalid address is provided as an associated value.
   case invalidAddress(String)
 
   /// An invalid address was given. A more descriptive string is provided as an associated value.
