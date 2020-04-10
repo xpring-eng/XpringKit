f0f8cc6a9fc9a7e552d79f44fb6be93a3a5d000c
Keefer Taylor <keefer.t@gmail.com>
2020-04-10 18:58:44 +0900

    Linter Improvements (#184)
    * Create a better linter config
    
    * additional improvements
    
    * fix indentation
    
    * update exclusions for test

--- XpringKit/ILP/auth/IlpCredentials.swift
+++ XpringKit/ILP/auth/IlpCredentials.swift
@@ -5,30 +5,30 @@ import SwiftGRPC
 /// going over the wire is prefixed with "Bearer "
 internal class IlpCredentials {
 
-    /// Unfortunately, Metadata is not an open class in SwiftGRPC, so we cannot extend it.
-    /// Instead, it is wrapped in IlpCredentials
-    private var metadata: Metadata
+  /// Unfortunately, Metadata is not an open class in SwiftGRPC, so we cannot extend it.
+  /// Instead, it is wrapped in IlpCredentials
+  private var metadata: Metadata
 
-    private let BEARER_PREFIX = "Bearer "
+  private let bearerPrefix = "Bearer "
 
-    /// Initialize a new IlpCredentials
-    /// self.metadata will be initialized and an Authorization header will be added to it.  The value
-    /// of that entry will be the bearerToken with "Bearer " prefix.
-    ///
-    /// - Parameters:
-    ///     - accessToken: An access token with no "Bearer " prefix
-    /// - Throws: if accessToken starts with "Bearer "
-    public init(_ accessToken: String) throws {
-        if accessToken.starts(with: BEARER_PREFIX) {
-            throw IlpError.invalidAccessToken
-        }
-
-        self.metadata = Metadata()
-        try metadata.add(key: "authorization", value: BEARER_PREFIX + accessToken)
+  /// Initialize a new IlpCredentials
+  /// self.metadata will be initialized and an Authorization header will be added to it.  The value
+  /// of that entry will be the bearerToken with "Bearer " prefix.
+  ///
+  /// - Parameters:
+  ///     - accessToken: An access token with no "Bearer " prefix
+  /// - Throws: if accessToken starts with "Bearer "
+  public init(_ accessToken: String) throws {
+    if accessToken.starts(with: bearerPrefix) {
+      throw IlpError.invalidAccessToken
     }
 
-    /// Get the Metadata to pass to network calls
-    public func getMetadata() -> Metadata {
-        return self.metadata
-    }
+    self.metadata = Metadata()
+    try metadata.add(key: "authorization", value: bearerPrefix + accessToken)
+  }
+
+  /// Get the Metadata to pass to network calls
+  public func getMetadata() -> Metadata {
+    return self.metadata
+  }
 }
