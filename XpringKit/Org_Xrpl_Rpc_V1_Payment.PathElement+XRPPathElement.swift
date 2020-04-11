230504ebcca06d6372673cf051a93ee6bcd59dd2
Amie Corso <acorso@ripple.com>
2020-04-11 08:27:20 -0700

    doc XRPPathElement

--- XpringKit/Org_Xrpl_Rpc_V1_Payment.PathElement+XRPPathElement.swift
+++ XpringKit/Org_Xrpl_Rpc_V1_Payment.PathElement+XRPPathElement.swift
@@ -1,4 +1,15 @@
+/// Conforms to XRPPathElement struct while providing an initializer that can construct an XRPPathElement
+/// from an Org_Xrpl_Rpc_V1_Payment.PathElement
 internal extension XRPPathElement {
+
+  /// Constructs an XRPPathElement from an Org_Xrpl_Rpc_V1_Payment.PathElement
+  /// - SeeAlso: [PathElement Protocol Buffer]
+  /// (https://github.com/ripple/rippled/blob/develop/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L227)
+  ///
+  /// - Parameters:
+  ///     - pathElement: a Org_Xrpl_Rpc_V1_Payment.PathElement (protobuf object) whose field values will be used to
+  ///             construct an XRPPathElement
+  /// - Returns: an XRPPathElement with its fields set via the analogous protobuf fields.
   init(pathElement: Org_Xrpl_Rpc_V1_Payment.PathElement) {
     self.account = pathElement.hasAccount ? pathElement.account.address : nil
     self.currency = pathElement.hasCurrency ? XRPCurrency(currency: pathElement.currency) : nil
