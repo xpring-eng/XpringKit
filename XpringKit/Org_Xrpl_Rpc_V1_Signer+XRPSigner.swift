2bab5ded8fd75b9c36cdb604a7b0a0ba0dc2af6b
Amie Corso <acorso@ripple.com>
2020-04-11 08:39:54 -0700

    doc XRPSigner

--- XpringKit/Org_Xrpl_Rpc_V1_Signer+XRPSigner.swift
+++ XpringKit/Org_Xrpl_Rpc_V1_Signer+XRPSigner.swift
@@ -1,4 +1,15 @@
+/// Conforms to XRPSigner struct while providing an initializer that can construct an XRPSigner
+/// from an Org_Xrpl_Rpc_V1_Signer
 internal extension XRPSigner {
+  
+  /// Constructs an XRPSigner from an Org_Xrpl_Rpc_V1_Signer
+  /// - SeeAlso: [Signer Protocol Buffer]
+  /// (https://github.com/ripple/rippled/blob/develop/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L90)
+  ///
+  /// - Parameters:
+  ///     - signer: a Org_Xrpl_Rpc_V1_Signer (protobuf object) whose field values will be used to
+  ///                 construct an XRPSigner
+  /// - Returns: an XRPSigner with its fields set via the analogous protobuf fields.
   init(signer: Org_Xrpl_Rpc_V1_Signer) {
     self.account = signer.account.value.address
     self.signingPublicKey = signer.signingPublicKey.value
--- XpringKit/XRPSigner.swift
+++ XpringKit/XRPSigner.swift
@@ -3,7 +3,13 @@ import Foundation
 /// Represents a signer of a transaction on the XRP Ledger.
 /// - SeeAlso: https://xrpl.org/transaction-common-fields.html#signers-field
 public struct XRPSigner: Equatable {
+  
+  /// The address associated with this signature, as it appears in the SignerList.
   public let account: Address
+  
+  /// The public key used to create this signature.
   public let signingPublicKey: Data
+  
+  /// A signature for this transaction, verifiable using the SigningPubKey.
   public let transactionSignature: Data
 }
