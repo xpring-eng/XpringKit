b96cb494d1d3fe9108f17a981efda53e8984ff70
Amie Corso <acorso@ripple.com>
2020-04-11 08:35:18 -0700

    doc XRPPayment

--- XpringKit/Org_Xrpl_Rpc_V1_Payment+XRPPayment.swift
+++ XpringKit/Org_Xrpl_Rpc_V1_Payment+XRPPayment.swift
@@ -1,6 +1,17 @@
 import Foundation
 
+/// Conforms to XRPPayment struct while providing an initializer that can construct an XRPPayment
+/// from an Org_Xrpl_Rpc_V1_Payment
 internal extension XRPPayment {
+  
+  /// Constructs an XRPPayment from an Org_Xrpl_Rpc_V1_Payment
+  /// - SeeAlso: [Payment Protocol Buffer]
+  /// (https://github.com/ripple/rippled/blob/develop/src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L224)
+  ///
+  /// - Parameters:
+  ///     - payment: a Org_Xrpl_Rpc_V1_Payment (protobuf object) whose field values will be used to
+  ///             construct an XRPPayment
+  /// - Returns: an XRPPayment with its fields set via the analogous protobuf fields.
   init?(payment: Org_Xrpl_Rpc_V1_Payment) {
     guard let amount = XRPCurrencyAmount(currencyAmount: payment.amount.value) else {
       return nil
