e195c465cfff38992473ad0cf1c96ca2f14890fd
Amie Corso <acorso@ripple.com>
2020-04-11 07:27:39 -0700

    doc XRPCurrencyAmount

--- XpringKit/Org_Xrpl_Rpc_V1_IssuedCurrencyAmount+XRPCurrencyAmount.swift
+++ XpringKit/Org_Xrpl_Rpc_V1_IssuedCurrencyAmount+XRPCurrencyAmount.swift
@@ -1,4 +1,13 @@
+/// Conforms to XRPCurrencyAmount struct while providing an initializer that can construct an XRPCurrencyAmount
+/// from an Org_Xrpl_Rpc_V1_CurrencyAmount
 internal extension XRPCurrencyAmount {
+  
+  /// Constructs an XRPCurrencyAmount from an Org_Xrpl_Rpc_V1_CurrencyAmount
+  /// - SeeAlso: [CurrencyAmount Protocol Buffer] (https://github.com/ripple/rippled/blob/develop/src/ripple/proto/org/xrpl/rpc/v1/amount.proto#L10)
+  ///
+  /// - Parameters:
+  ///     - currencyAmount: a CurrencyAmount (protobuf object) whose field values will be used to construct an XRPCurrencyAmount
+  /// - Returns: an XRPCurrencyAmount with its fields set via the analogous protobuf fields.
   init?(currencyAmount: Org_Xrpl_Rpc_V1_CurrencyAmount) {
     switch currencyAmount.amount {
     case .issuedCurrencyAmount(let issuedCurrency):
--- XpringKit/XRPCurrencyAmount.swift
+++ XpringKit/XRPCurrencyAmount.swift
@@ -1,7 +1,11 @@
-// An amount of currency on the XRP Ledger
-// - SeeAlso: https://xrpl.org/basic-data-types.html#specifying-currency-amounts
+/// An amount of currency on the XRP Ledger
+/// - SeeAlso: https://xrpl.org/basic-data-types.html#specifying-currency-amounts
 public struct XRPCurrencyAmount: Equatable {
-  // Mutually exclusive, only one of the below fields is set.
+  /// Mutually exclusive, only one of the below fields is set.
+  
+  /// An amount of XRP, specified in drops.
   public let drops: UInt64?
+  
+  /// An amount of an issued currency.
   public let issuedCurrency: XRPIssuedCurrency?
 }
