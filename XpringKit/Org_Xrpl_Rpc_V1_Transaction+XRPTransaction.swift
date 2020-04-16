1b1fcc22d9aeb5283c9ad044bbe38b914e61fafa
Amie Corso <acorso@ripple.com>
2020-04-16 13:15:43 -0700

    complete XRPTransaction field inits for hash timestamp deliveredAmount

--- XpringKit/Org_Xrpl_Rpc_V1_Transaction+XRPTransaction.swift
+++ XpringKit/Org_Xrpl_Rpc_V1_Transaction+XRPTransaction.swift
@@ -4,9 +4,9 @@ internal extension XRPTransaction {
   private static let javaScriptUtils = JavaScriptUtils()
 
   init?(getTransactionResponse: Org_Xrpl_Rpc_V1_GetTransactionResponse) {
-    
+
     let transaction: Org_Xrpl_Rpc_V1_Transaction = getTransactionResponse.transaction
-    
+
     self.hash = XRPTransaction.javaScriptUtils.toHex(bytes: getTransactionResponse.hash)
     self.account = transaction.account.value.address
     self.fee = transaction.fee.drops
@@ -32,15 +32,17 @@ internal extension XRPTransaction {
       // Unsupported transaction type.
       return nil
     }
-    
+
     // Transactions report their timestamps since the Ripple Epoch, which is 946,684,800 seconds
     // after the unix epoch. Convert transaction's timestamp to a unix timestamp.
     // - SeeAlso: https://xrpl.org/basic-data-types.html#specifying-time
     if getTransactionResponse.hasDate {
       let rippleTransactionDate = UInt64(getTransactionResponse.date.value)
-      self.timestamp = rippleTransactionDate + 946684800
+      self.timestamp = rippleTransactionDate + 946_684_800
+    } else {
+      self.timestamp = nil
     }
-    
+
     if getTransactionResponse.meta.hasDeliveredAmount && getTransactionResponse.meta.deliveredAmount.hasValue {
       let amountCase = getTransactionResponse.meta.deliveredAmount.value.amount
       switch amountCase {
@@ -51,6 +53,8 @@ internal extension XRPTransaction {
       case .none:
         self.deliveredAmount = nil
       }
+    } else {
+      self.deliveredAmount = nil
     }
   }
 }
