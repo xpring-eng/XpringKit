0ec59eaf6e7d58b5df3c91b4278aab4897255073
Amie Corso <acorso@ripple.com>
2020-04-16 08:37:33 -0700

    add fields to struct

--- XpringKit/XRPTransaction.swift
+++ XpringKit/XRPTransaction.swift
@@ -6,6 +6,7 @@ import Foundation
 // TODO(keefertaylor): Modify this object to use X-Address format.
 public struct XRPTransaction: Equatable {
   /// Common fields
+  public let hash: String
   public let account: Address
   public let accountTransactionID: Data?
   public let fee: UInt64
@@ -18,6 +19,8 @@ public struct XRPTransaction: Equatable {
   public let sourceTag: UInt32?
   public let transactionSignature: Data
   public let type: XRPTransactionType
+  public let timestamp: UInt64
+  public let deliveredAmount: String
 
   /// Transaction specific fields, only one of the following will be set.
   public let paymentFields: XRPPayment?
