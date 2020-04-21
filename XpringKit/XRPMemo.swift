1333eef69dab3a7fb44102d6b0b7cde211e43432
Amie Corso <acorso@ripple.com>
2020-04-21 07:17:14 -0700

    add '(Optional)' to XRPMemo class

--- XpringKit/XRPMemo.swift
+++ XpringKit/XRPMemo.swift
@@ -4,14 +4,14 @@ import Foundation
 /// - SeeAlso: https://xrpl.org/transaction-common-fields.html#memos-field
 public struct XRPMemo: Equatable {
 
-  /// Arbitrary hex value, conventionally containing the content of the memo.
+  /// (Optional) Arbitrary hex value, conventionally containing the content of the memo.
   public let data: Data?
 
-  /// Hex value representing characters allowed in URLs.
+  /// (Optional) Hex value representing characters allowed in URLs.
   /// Conventionally containing information on how the memo is encoded, for example as a MIME type.
   public let format: Data?
 
-  /// Hex value representing characters allowed in URLs.
+  /// (Optional) Hex value representing characters allowed in URLs.
   /// Conventionally, a unique relation (according to RFC 5988) that defines the format of this memo.
   public let type: Data?
 }
