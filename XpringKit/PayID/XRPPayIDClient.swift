df12d294fd98eb26d20e5a99bd7f7e56d4a549b2
Amie Corso <acorso@ripple.com>
2020-04-29 16:50:19 -0700

    fix bad merge

--- XpringKit/PayID/XRPPayIDClient.swift
+++ XpringKit/PayID/XRPPayIDClient.swift
@@ -1,16 +1,8 @@
-import Foundation
+//
+//  XRPPayIDClient.swift
+//  XpringKitTests_iOS
+//
+//  Created by Amie Corso on 4/29/20.
+//
 
-/// Provides functionality for XRP in the PayID protocol.
-public class XRPPayIDClient: PayIDClient {
-  
-  /// The XRP Ledger network that this client attaches to.
-  var xrplNetwork: XRPLNetwork
-  
-  /// Initialize a new XRPPayIDclient
-  ///
-  /// - Parameter xrplNetwork: The XRP Ledger network that this client attaches to.
-  init(xrplNetwork: XRPLNetwork) {
-    super.init(network: "xrpl-\(xrplNetwork)")
-    self.xrplNetwork = xrplNetwork
-  }
-}
+import Foundation
