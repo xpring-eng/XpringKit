659480d20d08c2cc9dae958bc64911ff85a0ca51
Amie Corso <acorso@ripple.com>
2020-05-04 13:36:59 -0700

    call network xrplNetwork

--- Tests/PayID/Fakes/FakeXRPPayIDClient.swift
+++ Tests/PayID/Fakes/FakeXRPPayIDClient.swift
@@ -3,15 +3,15 @@ import XpringKit
 
 /// Fakes a PayID client.
 public class FakeXRPPayIDClient: XRPPayIDClientProtocol {
-  public let network: XRPLNetwork
+  public let xrplNetwork: XRPLNetwork
   private let addressResult: Result<String, PayIDError>
 
   /// Initialize a new fake Pay ID client.
   ///
   /// - Parameter addressResult: The result that will be returned from calls to `xrpAddress(for:completion:)`.
-  public init(network: XRPLNetwork = .test, addressResult: Result<String, PayIDError>) {
+  public init(xrplNetwork: XRPLNetwork = .test, addressResult: Result<String, PayIDError>) {
     self.addressResult = addressResult
-    self.network = network
+    self.xrplNetwork = xrplNetwork
   }
 
   public func xrpAddress(
