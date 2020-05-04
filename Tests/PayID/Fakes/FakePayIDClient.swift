405ce235b3b0f1b91ceb6883394e9052361f0efa
Keefer Taylor <keefer.t@gmail.com>
2020-05-04 22:35:43 +0900

    Validate Networks in XpringClient (#201)
    * add API spec
    
    * update scripts
    
    * executable script
    
    * get things working e2e for codegen
    
    * remove temp swagger
    
    * add AlamoFire dependency
    
    * regenerate project
    
    * building project
    
    * Fix syntax in podfile
    
    * update code
    
    * ditch on tests
    
    * fix merge conflict
    
    * remove files
    
    * udpates
    
    * add network to XRP client
    
    * minor grammar updates

--- Tests/PayID/Fakes/FakePayIDClient.swift
+++ Tests/PayID/Fakes/FakePayIDClient.swift
@@ -3,14 +3,17 @@ import XpringKit
 
 /// Fakes a PayID client.
 public class FakePayIDClient: PayIDClientProtocol {
+  public let network: XRPLNetwork
+
   /// Results from method calls.
   private let addressResult: Result<String, PayIDError>
 
   /// Initialize a new fake Pay ID client.
   ///
   /// - Parameter addressResult: The result that will be returned from calls to `xrpAddress(for:completion:)`.
-  public init(addressResult: Result<String, PayIDError>) {
+  public init(network: XRPLNetwork = .test, addressResult: Result<String, PayIDError>) {
     self.addressResult = addressResult
+    self.network = network
   }
 
   public func xrpAddress(
