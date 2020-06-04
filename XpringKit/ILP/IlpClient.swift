0285dd2bb54612dd627f141d8ddc5ab11724db7c
Keefer Taylor <keefer.t@gmail.com>
2020-06-04 17:40:37 +0900

    Idiomatically Case ILP Classes (#232)
    * fix casing in ILP classes
    
    * Update CHANGELOG.md
    
    * Update README.md
    
    * Update config.yml
    
    * Add period
    
    * Reviewer suggested edits
    
    * Reviewer suggested edits
    
    * Reviewer suggested edits
    
    * Reviewer suggested edits
    
    * Reviewer suggested edits
    
    * Update CHANGELOG.md
    
    * Allow warnings about deprecated code
    
    Co-authored-by: Amie <acorso@ripple.com>

--- XpringKit/ILP/IlpClient.swift
+++ XpringKit/ILP/IlpClient.swift
@@ -1,4 +1,52 @@
 /// A client that can get balances and send ILP payments on a Xpring connector.
+public class ILPClient {
+
+  private let decoratedClient: ILPClientDecorator
+
+  /// Initialize a new client with a configured URL
+  ///
+  /// - Parameters:
+  ///     - grpcURL : The gRPC URL exposed by Hermes
+  public init(grpcURL: String) {
+    decoratedClient = DefaultILPClient(grpcURL: grpcURL)
+  }
+
+  /// Get the balance of the specified account on the connector.
+  ///
+  /// - Parameters:
+  ///     -  accountID The account ID to get the balance for.
+  ///     -  accessToken Authentication access token. Cannot start with "Bearer ".
+  /// - Returns: An AccountBalance with balance information of the specified account.
+  /// - Throws: An error if the given inputs were invalid, the account doesn't exist, or authentication failed.
+  public func getBalance(
+    for accountID: AccountID,
+    withAuthorization accessToken: AccessToken
+  ) throws -> AccountBalance {
+    return try decoratedClient.getBalance(for: accountID, withAuthorization: accessToken)
+  }
+
+  /// Send a payment from the given accountID to the destinationPaymentPointer payment pointer
+  ///
+  /// - Note: This method will not necessarily throw an exception if the payment failed.
+  ///         Payment status can be checked in PaymentResult.successfulPayment.
+  /// - Parameters:
+  ///     -  paymentRequest: A PaymentRequest with options for a payment
+  ///     -  accessToken : access token of the sender. Can not start with "Bearer "
+  /// - Returns: A PaymentResult with details about the payment.
+  /// - Throws: An error if the given inputs were invalid.
+  public func sendPayment(
+    _ paymentRequest: PaymentRequest,
+    withAuthorization accessToken: AccessToken
+  ) throws -> PaymentResult {
+    return try decoratedClient.sendPayment(
+      paymentRequest,
+      withAuthorization: accessToken
+    )
+  }
+}
+
+/// A client that can get balances and send ILP payments on a Xpring connector.
+@available(*, deprecated, message: "Please use the idiomatically named ILPClient class instead.")
 public class IlpClient {
 
   private let decoratedClient: IlpClientDecorator
