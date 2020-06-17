8846d81f465bbf4b15f28e78a9a48cd590249faa
Keefer Taylor <keefer.t@gmail.com>
2020-06-17 19:48:58 +0900

    wip

--- XpringKit/PayID/PayIDClient.swift
+++ XpringKit/PayID/PayIDClient.swift
@@ -19,6 +19,8 @@ public class PayIDClient {
 
   /// Initialize a new PayID client.
   ///
+  /// - Warning: This initializer is deprecated. `PayIDClient`s will not be bound to a network in the future.
+  ///
   /// - Parameter network: The network that addresses will be resolved on.
   ///
   /// - Note: Networks in this constructor take the form of an asset and an optional network (<asset>-<network>).
@@ -29,8 +31,25 @@ public class PayIDClient {
   ///   - ach
   ///
   //  TODO: Link a canonical list at payid.org when available.
+  @available(*, deprecated, message: "Use `init` instead.")
   public init(network: String) {
     self.network = network
+  }
+
+  /// Initialize a new PayID client.
+  ///
+  /// - Note: Networks in this constructor take the form of an asset and an optional network (<asset>-<network>).
+  /// For instance:
+  ///   - xrpl-testnet
+  ///   - xrpl-mainnet
+  ///   - eth-rinkeby
+  ///   - ach
+  ///
+  //  TODO: Link a canonical list at payid.org when available.
+  public init() {
+    // Stub out the network parameter with an empty string to avoid having to use an optional.
+    // TODO(keefertaylor): Remove this and the `network` iVar when deprecated methods are removed.
+    self.network = ""
   }
 
   /// Resolve the given PayID to an address.
@@ -39,8 +58,25 @@ public class PayIDClient {
   /// - Parameter completion: A closure called with the result of the operation.
   /// - Returns: An address representing the given PayID.
   // TODO(keefertaylor): Make this API synchronous to mirror functionality provided by ILP / XRP.
+  @available(*, deprecated, message: "Use `address(for:on:completion)` instead.")
   public func address(
     for payID: String,
+    completion: @escaping (Result<CryptoAddressDetails, PayIDError>) -> Void
+  ) {
+    // Pass through to the non-deprecated method using the iVar.
+    address(for: payID, on: self.network, completion: completion)
+  }
+
+  /// Resolve the given PayID to an address on the given network.
+  ///
+  /// - Parameter payID: The PayID to resolve for an address.
+  /// - Parameter network: The network to resolve on.
+  /// - Parameter completion: A closure called with the result of the operation.
+  /// - Returns: An address representing the given PayID.
+  // TODO(keefertaylor): Make this API synchronous to mirror functionality provided by ILP / XRP.
+  public func address(
+    for payID: String,
+    on network: String,
     completion: @escaping (Result<CryptoAddressDetails, PayIDError>) -> Void
   ) {
     guard let payIDComponents = PayIDUtils.parse(payID: payID) else {
