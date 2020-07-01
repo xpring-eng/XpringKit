a0d7a42e9783e625b354c84eb779e6cd2524ec96
Keefer Taylor <keefer.t@gmail.com>
2020-07-01 13:39:27 -0700

    More tests

--- Tests/PayID/PayIDClientTest.swift
+++ Tests/PayID/PayIDClientTest.swift
@@ -123,7 +123,7 @@ final class PayIDClientTest: XCTestCase {
     }
   }
 
-  func testAddressWithSuccessResponseUnexpectedResponseFormat() {
+  func testAddressWithSuccessResponseUnknownResponseFormat() {
     // GIVEN mocked networking to return a 200 with an unexpected format..
     let payID = "georgewashington$xpring.money"
     let malformedResponse = [
@@ -144,8 +144,82 @@ final class PayIDClientTest: XCTestCase {
     case .success:
       XCTFail("Should not resolve an address")
     case .failure(let error):
-//      XCTAssertEqual(error, PayIDError.unknown(error: ))
+      guard case .unknown = error else {
+        XCTFail("Should return unknown error type")
+        return
+      }
     }
   }
 
+  func testAllAddressesWithInvalidPayID() {
+    // GIVEN an invalid PayID.
+    let invalidPayID = "xpring.money/georgewashington" // Does not contain '$'
+
+    // WHEN all addresses are requested.
+    let result = payIDClient.allAddresses(for: invalidPayID)
+
+    // THEN the response is an invalid PayID error.
+    switch result {
+    case .success:
+      XCTFail("Should not resolve an address for an invalid PayID.")
+    case .failure(let error):
+      XCTAssertEqual(error, PayIDError.invalidPayID(payID: invalidPayID))
+    }
+  }
+
+  func testAllAddressesForPayIDMatchFound() {
+    // GIVEN a valid PayID and mocked networking to return a set of matches for the PayID.
+    let payID = "georgewashington$xpring.money"
+    let matches = PaymentInformation(
+      addresses: [
+        PayIDAddress(
+          paymentNetwork: paymentNetwork,
+          addressDetailsType: "CryptoAddressDetails",
+          addressDetails: CryptoAddressDetails(
+            address: "X7cBcY4bdTTzk3LHmrKAK6GyrirkXfLHGFxzke5zTmYMfw4"
+          )
+        ),
+        PayIDAddress(
+          paymentNetwork: paymentNetwork,
+          addressDetailsType: "CryptoAddressDetails",
+          addressDetails: CryptoAddressDetails(
+            address: "XV5sbjUmgPpvXv4ixFWZ5ptAYZ6PD28Sq49uo34VyjnmK5H"
+          )
+        )
+      ]
+    )
+    let matchesBytes = try! jsonEncoder.encode(matches)
+    FakeURLProtocol.responseWithStatusCode(
+      code: 200,
+      asciiString: String(data: matchesBytes, encoding: .utf8)!
+    )
+
+    // WHEN all addresses are resolved.
+    let result = payIDClient.allAddresses(for: payID)
+
+    // THEN the returned data is as expected.
+    switch result {
+    case .success(let results):
+      XCTAssertEqual(results.count, 2)
+    case .failure:
+      XCTFail("Should have resolved a set of addresses")
+    }
+  }
+
+  func testAllAddressesWithMatchNotFound() {
+    // GIVEN mocked networking to return a 404 for a request to resolve a PayID.
+    let payID = "georgewashington$xpring.money"
+    FakeURLProtocol.responseWithStatusCode(code: 404, asciiString: "not found")
+
+    // WHEN an address is resolved.
+    let result = payIDClient.allAddresses(for: payID)
+
+    // THEN the result is a not found error.
+    switch result {
+    case .success:
+      XCTFail("Should not resolve an address")
+    case .failure(let error):
+      XCTAssertEqual(error, PayIDError.mappingNotFound(payID: payID, network: "payid"))
+    }
+  }
 }
