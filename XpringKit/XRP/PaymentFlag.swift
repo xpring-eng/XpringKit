0965d85bcbc0f72df28f5cd4d399b4fbe51b5b0f
Amie Corso <acorso@ripple.com>
2020-08-03 10:03:41 -0700

    actually call it PaymentFlag

--- XpringKit/XRP/PaymentFlag.swift
+++ XpringKit/XRP/PaymentFlag.swift
@@ -2,11 +2,11 @@
 ///
 /// These are only flags which are utilized in Xpring SDK.
 /// For the complete list of payment flags, see https://xrpl.org/payment.html#payment-flags
-public struct PaymentFlags: OptionSet {
+public struct PaymentFlag: OptionSet {
   public let rawValue: UInt32
 
   /// Flag values.
-  public static let tfPartialPayment = PaymentFlags(rawValue: 1 << 17)
+  public static let tfPartialPayment = PaymentFlag(rawValue: 1 << 17)
 
   /// Initialize a new PaymentFlag
   public init(rawValue: UInt32) {
