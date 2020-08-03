a76d024fbbbc7450a4e906d9b390b92da6be86a4
Amie Corso <acorso@ripple.com>
2020-08-03 11:57:48 -0700

    document individual fields of TransactionResult

--- XpringKit/XRP/Model/TransactionResult.swift
+++ XpringKit/XRP/Model/TransactionResult.swift
@@ -1,17 +1,16 @@
 /// Represents the outcome of submitting an XRPL transaction.
 public struct TransactionResult {
+  
+  /// The identifying hash of the transaction.
   public let hash: TransactionHash
+  
+  /// The TransactionStatus indicating the outcome of this transaction.
   public let status: TransactionStatus
+  
+  /// Whether this transaction is included in a validated ledger.  The transactions status is only final if this field is true.
   public let validated: Bool
 
   /// Create a new TransactionResult object.
-  ///
-  /// - Parameters:
-  ///   - hash: The identifying hash of the transaction.
-  ///   - status: The TransactionStatus indicating the outcome of this transaction.
-  ///   - validated: Whether this transaction is included in a validated ledger.
-  ///                The transactions status is only final if this field is true.
-  ///
   public init(hash: TransactionHash, status: TransactionStatus, validated: Bool) {
     self.hash = hash
     self.status = status
