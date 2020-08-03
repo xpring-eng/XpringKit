c7e2b81afe19541c9406997561ad7a0967b7e1bf
Amie Corso <acorso@ripple.com>
2020-08-03 11:01:16 -0700

    rename AccountSet values

--- XpringKit/XRP/Model/AccountSetFlag.swift
+++ XpringKit/XRP/Model/AccountSetFlag.swift
@@ -3,13 +3,13 @@
 /// These are only flags which are utilized in Xpring SDK.
 /// For the complete list of AccountSet flags, see https://xrpl.org/accountset.html#accountset-flags
 enum AccountSetFlag: UInt32 {
-  case ASF_REQUIRE_DEST = 1
-  case ASF_REQUIRE_AUTH = 2
-  case ASF_DISALLOW_XRP = 3
-  case ASF_DISABLE_MASTER = 4
-  case ASF_ACCOUNT_TXN_ID = 5
-  case ASF_NO_FREEZE = 6
-  case ASF_GLOBAL_FREEZE = 7
-  case ASF_DEFAULT_RIPPLE = 8
-  case ASF_DEPOSIT_AUTH = 9
+  case asfRequireDest = 1
+  case asfRequireAuth = 2
+  case asfDisallowXRP = 3
+  case asfDisableMaster = 4
+  case asfAccountTxnId = 5
+  case asfNoFreeze = 6
+  case asfGlobalFreeze = 7
+  case asfDefaultRipple = 8
+  case asfDepositAuth = 9
 }
