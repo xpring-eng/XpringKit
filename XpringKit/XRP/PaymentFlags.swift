a9a666e3f14e051edbde150654acc20dbb1e0f5c
Amie Corso <acorso@ripple.com>
2020-08-03 10:02:12 -0700

    rename RippledFlags to PaymentFlag

---
+++ XpringKit/XRP/PaymentFlags.swift
@@ -0,0 +1,15 @@
+/// Flags used in payment transactions.
+///
+/// These are only flags which are utilized in Xpring SDK.
+/// For the complete list of payment flags, see https://xrpl.org/payment.html#payment-flags
+public struct PaymentFlags: OptionSet {
+  public let rawValue: UInt32
+
+  /// Flag values.
+  public static let tfPartialPayment = PaymentFlags(rawValue: 1 << 17)
+
+  /// Initialize a new PaymentFlag
+  public init(rawValue: UInt32) {
+    self.rawValue = rawValue
+  }
+}
