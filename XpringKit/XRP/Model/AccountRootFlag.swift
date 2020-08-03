b689d2772b5f9c0a01a31662ecd35c7513b0e3a5
Amie Corso <acorso@ripple.com>
2020-08-03 11:59:55 -0700

    another line length vio

--- XpringKit/XRP/Model/AccountRootFlag.swift
+++ XpringKit/XRP/Model/AccountRootFlag.swift
@@ -1,6 +1,7 @@
 /// There are several options which can be either enabled or disabled for an XRPL account using AccountSet transactions.
-/// These options are indicated in a bitmap of AccountRoot flags.  The bit values for the flags in the ledger are different
-/// from the values used to enable or disable those flags in a transaction.  Ledger flags have names that begin with lsf.
+/// These options are indicated in a bitmap of AccountRoot flags.  The bit values for the flags in the ledger are
+/// different from the values used to enable or disable those flags in a transaction.
+/// Ledger flags have names that begin with lsf.
 ///
 /// These are only flags which are utilized in Xpring SDK.
 /// For the complete list of AccountRoot flags, see https://xrpl.org/accountroot.html#accountroot-flags
