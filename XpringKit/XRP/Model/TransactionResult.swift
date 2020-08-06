a046059a4b800a93e4a82a80a1ca90f885badb17
Amie Corso <acorso@ripple.com>
2020-08-06 13:49:42 -0700

    remove init for TransactionResult

--- XpringKit/XRP/Model/TransactionResult.swift
+++ XpringKit/XRP/Model/TransactionResult.swift
@@ -10,11 +10,4 @@ public struct TransactionResult {
   /// Whether this transaction is included in a validated ledger.
   /// The transactions status is only final if this field is true.
   public let validated: Bool
-
-  /// Create a new TransactionResult object.
-  public init(hash: TransactionHash, status: TransactionStatus, validated: Bool) {
-    self.hash = hash
-    self.status = status
-    self.validated = validated
-  }
 }
