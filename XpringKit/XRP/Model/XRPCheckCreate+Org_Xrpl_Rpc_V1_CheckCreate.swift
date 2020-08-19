fd7b9790b348a95bfda69f764380de9e3759654d
Amie Corso <acorso@ripple.com>
2020-08-19 09:22:40 -0700

    XRPCheckCreate struct and extension

---
+++ XpringKit/XRP/Model/XRPCheckCreate+Org_Xrpl_Rpc_V1_CheckCreate.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+/// Conforms to XRPCheckCreate struct while providing an initializer that can construct an XRPCheckCreate
+/// from an Org_Xrpl_Rpc_V1_CheckCreate
+internal extension XRPCheckCreate {
+
+  /// Constructs an XRPCheckCreate from an Org_Xrpl_Rpc_V1_CheckCreate
+  /// - SeeAlso: [CheckCreate Protocol Buffer]
+  /// (https://github.com/ripple/rippled/blob/3d86b49dae8173344b39deb75e53170a9b6c5284/
+  /// src/ripple/proto/org/xrpl/rpc/v1/transaction.proto#L145)
+  ///
+  /// - Parameters:
+  ///     - checkCreate: an Org_Xrpl_Rpc_V1_CheckCreate (protobuf object) whose field values will be used to
+  ///             construct an XRPCheckCreate
+  /// - Returns: an XRPCheckCreate with its fields set via the analogous protobuf fields.
+  init?(checkCreate: Org_Xrpl_Rpc_V1_CheckCreate, xrplNetwork: XRPLNetwork) {
+    let destination = checkCreate.destination.value.address
+    let destinationTag = checkCreate.destinationTag.value
+
+    // must be successfully convertable to an X-Address
+    guard let destinationXAddress = Utils.encode(
+      classicAddress: destination,
+      tag: destinationTag,
+      isTest: xrplNetwork == XRPLNetwork.test || xrplNetwork == XRPLNetwork.dev
+      ) else {
+      return nil
+    }
+    self.destinationXAddress = destinationXAddress
+    
+    // sendMax is required
+    let sendMaxCurrencyAmount = checkCreate.sendMax.value
+    guard let sendMax = XRPCurrencyAmount(currencyAmount: sendMaxCurrencyAmount) else {
+      return nil
+    }
+    self.sendMax = sendMax
+    
+    self.expiration = checkCreate.expiration.value
+    self.invoiceId = String(decoding: checkCreate.invoiceID.value, as: UTF8.self)
+  }
+}
---
+++ XpringKit/XRP/Model/XRPCheckCreate.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+/// Represents a CheckCreate transaction on the XRP Ledger.
+///
+/// A CheckCreate transaction creates a Check object in the ledger, which is a deferred payment that can be cashed
+/// by its intended destination.  The sender of this transaction is the sender of the Check.
+///
+/// - SeeAlso: https://xrpl.org/checkcreate.html
+public struct XRPCheckCreate: Equatable {
+  
+  /// The unique address and (optional) destination tag of the account that can cash the Check,
+  /// encoded as an X-address.
+  /// - SeeAlso:  https://xrpaddress.info
+  public let destinationXAddress: String
+
+  /// Maximum amount of source currency the Check is allowed to debit the sender, including transfer fees on non-XRP currencies.
+  /// The Check can only credit the destination with the same currency (from the same issuer, for non-XRP currencies).
+  /// For non-XRP amounts, the nested field names MUST be lower-case.
+  public let sendMax: XRPCurrencyAmount
+
+  /// (Optional) Time after which the Check is no longer valid, in seconds since the Ripple Epoch.
+  public let expiration: UInt32?
+
+  /// (Optional) Arbitrary 256-bit hash representing a specific reason or identifier for this Check.
+  public let invoiceId: String?
+}
