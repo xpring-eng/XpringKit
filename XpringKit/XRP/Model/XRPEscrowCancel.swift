c4dafc1adfffcdc35364e1f2d88d7389ba6a6312
Amie Corso <acorso@ripple.com>
2020-08-20 08:50:39 -0700

    XRPEscrowCancel struct and extension

---
+++ XpringKit/XRP/Model/XRPEscrowCancel.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+/// Represents an EscrowCancel transaction on the XRP Ledger.
+///
+/// An EscrowCancel transaction returns escrowed XRP to the sender.
+/// - SeeAlso: https://xrpl.org/escrowcancel.html
+public struct XRPEscrowCancel: Equatable {
+
+  /// Address of the source account that funded the escrow payment, encoded as an X-address.
+  /// - seeAlso: https://xrpaddress.info
+  public let ownerXAddress: String
+  
+  /// Transaction sequence of EscrowCreate transaction that created the escrow to cancel.
+  public let offerSequence: UInt32
+}
