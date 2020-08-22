d9cacc8b5de518c6668173c975b54eb1bcdb0572
Amie Corso <acorso@ripple.com>
2020-08-21 18:16:00 -0700

    test objects and tests

--- XpringKit/XRP/Model/XRPOfferCancel.swift
+++ XpringKit/XRP/Model/XRPOfferCancel.swift
@@ -5,7 +5,7 @@ import Foundation
 /// An OfferCancel transaction removes an Offer object from the XRP Ledger.
 /// - SeeAlso: https://xrpl.org/offercancel.html
 public struct XRPOfferCancel: Equatable {
-  
+
   /// The sequence number of a previous OfferCreate transaction.
   /// If specified, cancel any offer object in the ledger that was created by that transaction.
   /// It is not considered an error if the offer specified does not exist.
