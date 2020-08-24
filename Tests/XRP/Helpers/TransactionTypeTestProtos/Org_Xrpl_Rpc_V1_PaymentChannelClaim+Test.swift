3c8807fd369ac4ac258d8c62404c0df1ebff618f
Amie Corso <acorso@ripple.com>
2020-08-24 11:24:24 -0700

    start test objects

---
+++ Tests/XRP/Helpers/TransactionTypeTestProtos/Org_Xrpl_Rpc_V1_PaymentChannelClaim+Test.swift
@@ -0,0 +1,13 @@
+import XpringKit
+
+extension Org_Xrpl_Rpc_V1_PaymentChannelClaim {
+  public static let testPaymentChannelClaimAllFields = Org_Xrpl_Rpc_V1_PaymentChannelClaim.with {
+    $0.channel = Org_Xrpl_Rpc_V1_Channel.with {
+      
+    }
+    //$0.balance =
+    //$0.amount =
+    //$0.paymentChannelSignature =
+    //$0.publicKey =
+  }
+}
