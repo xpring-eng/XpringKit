49d4c1433b86c5e96f80a188e0e24f585b06b7d3
Amie Corso <acorso@ripple.com>
2020-08-25 08:43:36 -0700

    send it

--- Tests/XRP/Helpers/String+Test.swift
+++ Tests/XRP/Helpers/String+Test.swift
@@ -6,4 +6,5 @@ extension String {
   static let testAssetCode = "XRP"
   static let testDomainValue = "testdomain"
   static let testClassicAddress = "rPEPPER7kfTD9w2To4CQk6UCfuHM9c6GDY"
+  static let testRegularKey = "rAR8rR8sUkBoCZFawhkWzY4Y5YoyuznwD"
 }
